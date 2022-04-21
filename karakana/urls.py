7f4e7c710b4a9e5df40181daa5adad1a17babd11
John-Kimani <kimwaweru100@gmail.com>
2022-04-21 12:49:10 +0300

    Dev: Creates views and urls Creates views and urls patterns for products application

--- karakana/urls.py
+++ karakana/urls.py
@@ -19,5 +19,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', include('main.urls'))
+    # main application
+    path('', include('main.urls')),
+
+    #produts application
+    path('products/', include('products.urls'))
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+def product_list(request):
+    '''
+    Main/homepage view function
+    '''
+    return render(request, 'products/products.html')
+
+def add_to_cart(request):
+    '''
+    About us view function
+    '''
+    return render(request, 'products/cart.html')
+
+
+def checkout(request):
+    '''
+    Contact us view function
+    '''
+    return render(request, 'products/checkout.html')
