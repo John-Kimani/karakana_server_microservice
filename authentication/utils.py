1e6037beda184667a21bb76cd2c4880e2368f51a
John-Kimani <kimwaweru100@gmail.com>
2023-02-28 14:19:06 +0300

    View: Create verify email class

---
+++ authentication/utils.py
@@ -0,0 +1,7 @@
+class Util:
+    '''
+        Method to use this class method without instanciating the class itself
+    '''
+    @staticmethod
+    def send_email(data):
+        pass
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import RegisterSerializer
+from .models import User
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_site
 
 class RegisterView(generics.GenericAPIView):
 
@@ -18,4 +22,25 @@ class RegisterView(generics.GenericAPIView):
 
             user_data = serializer.data
 
+            user = User.objects.get(email=user_data['email'])
+
+            token = RefreshToken.for_user(user)
+
+            current_site = get_current_site(request)
+
+            relativeLink=''
+
+            data = {
+                'domain': current_site.domain,
+
+            }
+
+            ## Marked as a static method to allow direct injection
+            Util.send_email(data=data)
+
             return Response(data=user_data, status=status.HTTP_201_CREATED)
+        
+
+class VerifyEmail(generics.GenericAPIView):
+    def get(self):
+        pass
